b9c787573dece76af47db0cd26e8ef029713eada
Alberto Zúñiga <albertozuniga@Albertos-MacBook-Pro.local>
2025-11-24 12:07:44 -0300

    feat: add cart and past orders templates, update navbar for navigation

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.auth_routes import auth_bp
 from app.routes.main_routes import main_bp
 from app.routes.product_routes import product_bp
+from app.routes.cart_routes import cart_bp
 
-blueprints = [main_bp, auth_bp, product_bp]
+blueprints = [main_bp, auth_bp, product_bp, cart_bp]
